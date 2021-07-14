2634ce45390d5096a46a3fe0d59b5a283b99ec78
Robertoskr <77611438+Robertoskr@users.noreply.github.com>
2021-07-14 23:49:09 +0200

    Update graficar.cpp

--- graficar.cpp
+++ graficar.cpp
@@ -5,6 +5,21 @@ using namespace std;
 double fCirculo(int x, int size){
     return sqrt(pow(size, 2) - pow(x, 2));
 }
+double squareX(int x){
+    return pow(x, 2);
+};
+double cubeX(int x){
+    return pow(x, 3);
+}
+double sqrtX(int x){
+    return sqrt(x);
+}
+double randomFunction(int x){
+    return pow(x, 3) - pow(x, 2) + 6;
+}
+double rationalX(int x){ // doesnt work because return a lot of decimals
+    return 1/x;
+}
 
 void pasarCoordenadas(int arr[], int size){
     int counter = 0;
@@ -13,30 +28,26 @@ void pasarCoordenadas(int arr[], int size){
             int x = b;
             int y = size / 2 - i;
             double height;
-            height = fCirculo(x, size / 2);
-            //cout << height << '-' << double(y) << ' ';
-            double diff = height - double(y);
-            double diff2 = height + double(y);
-            if (((diff < 0 && diff > 0.5) || (diff2 < 0 && diff2 > 0.5) ) || ((diff > 0 && diff < 0.5) || (diff2 > 0 && diff2 < 0.5)) || diff == 0 || diff2 == 0){
-                cout << " + ";
+            height = randomFunction(x);
+            double diff;
+            diff = height - double(y);
+            if ((diff < 0 && diff > 0.5 ) || (diff > 0 && diff < 0.5) || diff == 0){
+                cout << "+";
                 arr[counter] = 1;
             }else{
-                cout << ' ';
-                arr[counter] = 0;
+                if(y == 0){
+                    cout << " - ";
+                }else if(x == 0){
+                    cout << " | ";
+                }
+                else{
+                    cout << ' ';
+                    arr[counter] = 0;
+                }
             }
+
             counter++;
         }
             cout << endl;
     }
-}
-
-
-
-int main(){
-    int size; //Tamaño de la matriz
-    cout << "Whats the radius of the circle?: " << endl;
-    cin >> size;
-    int *p;
-    p = new int[size * size];
-    pasarCoordenadas(p, size);
 }
